d6235ec223729c512d3629b4a31ab86eb3e2255e
Eduardo Bouças <mail@eduardoboucas.com>
2025-03-08 23:59:54 +0000

    Update page.js

--- src/app/test/[slug]/page.js
+++ src/app/test/[slug]/page.js
@@ -2,10 +2,16 @@ export const revalidate = 3600; // Cache for 1 hour
 
 export default async function Page({ params }) {
   const { slug } = await params;
+
+  const res = await fetch("https://example.com", {
+    next: { revalidate: 3600 }
+  });
+  await res.text();
+
   return (
     <ul>
       <li>Slug: {slug}</li>
-      <li>Generation timestamp: {Date.now()}</li>
+      <li>Date: {res.headers.get("date")}</li>
     </ul>
   );
 }
