fea772178205eb0d95f0ac8d5e806c8a94c35de3
Eduardo Bouças <mail@eduardoboucas.com>
2025-03-09 00:34:03 +0000

    Update page.js

--- src/app/test/[slug]/page.js
+++ src/app/test/[slug]/page.js
@@ -1,13 +1,17 @@
-export const revalidate = 3600;
-export const dynamicParams = true;
-
 export default async function Page({ params }) {
   const { slug } = await params;
+
+  const res = await fetch(`https://example.com/${slug}`, {
+    next: {
+      revalidate: 3600
+    }
+  });
+  await res.text();
 
   return (
     <ul>
       <li>Slug: {slug}</li>
-      <li>Date: {Date.now()}</li>
+      <li>Date: {res.headers.get("date")}</li>
     </ul>
   );
 }
